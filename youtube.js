e979a8e395974aab8b586ae65d00476ba7adf05f
JayBroSobol <44853264+JayBroSobol@users.noreply.github.com>
2021-02-23 17:03:34 -0700

    Delete youtube.js

--- youtube.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'youtube',
-    description: "send you the yt link",
-    execute(message, args){
-        message.channel.send('https://www.youtube.com/channel/UC3SziOf56k44K9UxDrGXFqQ');
-    }
-}
